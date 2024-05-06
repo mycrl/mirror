f855bcaf0e6bd6c4d800044fd1de13f0336aa170
Lazy Panda <lepidodendraceae@gmail.com>
2024-05-07 01:55:54 +0800

    Update build.rs

--- codec/build.rs
+++ codec/build.rs
@@ -150,19 +150,11 @@ impl Settings {
 
 #[cfg(target_os = "windows")]
 fn find_ffmpeg_prefix(out_dir: &str) -> anyhow::Result<(Vec<String>, Vec<String>)> {
-    if !is_exsit(&join(out_dir, "7z.exe").unwrap()) {
-        exec(
-            "Invoke-WebRequest -Uri https://www.7-zip.org/a/7zr.exe -OutFile 7z.exe",
-            out_dir,
-        )
-        .expect("Unable to download 7z cli exe.");
-    }
-
-    let ffmpeg_prefix = join(out_dir, "ffmpeg-6.1.1-full_build-shared").unwrap();
+    let ffmpeg_prefix = join(out_dir, "ffmpeg-6.1.1").unwrap();
     if !is_exsit(&ffmpeg_prefix) {
         exec(
             "Invoke-WebRequest \
-                        -Uri https://github.com/mycrl/distributions/releases/download/distributions/ffmpeg-6.1.1-full_build-shared.7z \
+                        -Uri https://github.com/mycrl/distributions/releases/download/distributions/ffmpeg-6.1.1.zip \
                         -OutFile ffmpeg.7z; \
                      ./7z.exe x ffmpeg.7z -aoa; \
                      del ffmpeg.7z",
