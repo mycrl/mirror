7a23af6271ca70dabaeab6cd90831b33bc759c08
Lazy Panda <lepidodendraceae@gmail.com>
2024-05-21 22:09:31 +0800

    Update simple.rs

--- rist/examples/simple.rs
+++ rist/examples/simple.rs
@@ -6,16 +6,15 @@ use std::{
 use rist::{Receiver, Sender};
 
 fn main() -> anyhow::Result<()> {
-    thread::spawn(|| {
-        let receiver = Receiver::new("239.0.0.1:8084".parse()?)?;
+    let mut sender = Sender::new("239.0.0.1:8084".parse()?)?;
+    let receiver = Receiver::new("239.0.0.1:8084".parse()?)?;
+
+    thread::spawn(move || {
         while let Some(packet) = receiver.read() {
             println!("size={}", packet.as_slice().len())
         }
-
-        Ok::<(), anyhow::Error>(())
     });
 
-    let mut sender = Sender::new("239.0.0.1:8084".parse()?)?;
     let buf = [0u8; 1000];
     loop {
         sender.send(&buf)?;
