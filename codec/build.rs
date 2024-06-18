81a6e0d83d65e926b4f52ebad3142b403cc92ba4
Lazy Panda <lepidodendraceae@gmail.com>
2024-06-19 07:13:58 +0800

    Update build.rs

--- codec/build.rs
+++ codec/build.rs
@@ -63,35 +63,6 @@ fn main() -> anyhow::Result<()> {
     println!("cargo:rerun-if-changed=./build.rs");
 
     let settings = Settings::build()?;
-    if !is_exsit(&join(
-        &settings.out_dir,
-        if cfg!(target_os = "windows") {
-            "yuv.lib"
-        } else {
-            "libyuv.a"
-        },
-    )?) {
-        if cfg!(target_os = "windows") {
-            exec("Invoke-WebRequest \
-                -Uri https://github.com/mycrl/distributions/releases/download/distributions/yuv-windows-x64.lib \
-                -OutFile yuv.lib", &settings.out_dir)?;
-        } else {
-            exec(
-                "wget \
-                https://github.com/mycrl/libyuv-rs/releases/download/v0.1.2/libyuv-linux-x86_64.a \
-                -O libyuv.a",
-                &settings.out_dir,
-            )?;
-        }
-    }
-
-    if !is_exsit(&join(&settings.out_dir, "./libyuv")?) {
-        exec(
-            "git clone --branch stable https://chromium.googlesource.com/libyuv/libyuv",
-            &settings.out_dir,
-        )?;
-    }
-
     cc::Build::new()
         .cpp(true)
         .std("c++20")
@@ -126,7 +97,6 @@ fn main() -> anyhow::Result<()> {
     println!("cargo:rustc-link-lib=avcodec");
     println!("cargo:rustc-link-lib=avutil");
     println!("cargo:rustc-link-lib=codec");
-    println!("cargo:rustc-link-lib=yuv");
     Ok(())
 }
 
